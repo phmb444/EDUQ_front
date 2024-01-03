dd09bbba9a67768177c65193030219317ee20b69
Pedro Haubert <pedrohaubert7@gmail.com>
2024-01-03 17:34:39 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import '../app.css';
+</script>
+
+<header class="w-full h-20 flex px-8 items-center justify-between">
+	<a href="/"><img src="eduq_horizontal.png" alt="" class="md:h-10 h-6" /></a>
+	<nav class="hidden lg:block">
+		<ul class="flex xl:space-x-40 space-x-20 font-bold xl:pl-20 justify-center">
+			<li>
+				<a href="." class="p-1 hover:border-b-2 hover:border-black ease-out duration-75">Conheça</a>
+			</li>
+			<li>
+				<a href="." class="p-1 hover:border-b-2 hover:border-black ease-out duration-75">Contato</a>
+			</li>
+			<li>
+				<a href="." class="p-1 hover:border-b-2 hover:border-black ease-out duration-75">Tutorial</a
+				>
+			</li>
+		</ul>
+	</nav>
+	<div class="space-x-2 font-bold">
+		<a
+			href="/login"
+			class="md:px-6 md:py-3 px-2 py-2 border-2 border-blue-500 rounded-full ease-out duration-300 hover:bg-blue-500 hover:text-white"
+			>Login</a
+		>
+		<a
+			href="/register"
+			class="md:px-6 md:py-3 px-2 py-2 border-2 border-green-500 rounded-full ease-out duration-300 hover:bg-green-500 hover:text-white"
+			>Cadastre-se</a
+		>
+	</div>
+</header>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+
+<main class="w-full px-8 ">
+	<section class="w-full bg-yellow-100 px-8 py-8 xl:px-24 xl:py-24 rounded-3xl mb-12">
+		<div class="flex items-center h-full">
+			<div class="w-1/2 h-full lg:w-4/6 flex flex-col justify-between ">
+				<div class="h-full">
+					<h1 class="text-2xl sm:text-4xl md:text-5xl lg:text-6xl lg:mb-12 font-extrabold mb-4">
+						Conheça uma nova forma de estudar
+					</h1>
+					<p class="font-bold text-sm lg:text-xl hidden lg:block">
+						Explore um universo de conhecimento com nossa plataforma, projetada para tornar a
+						educação envolvente e personalizada. Nossa plataforma conta com digitalização de
+						materiais didáticos, sistema de questionários e um assistente de inteligência
+						artificial, permitindo os professores criarem seu próprio material didático de forma
+						simples e auxiliando os alunos no processo de aprendizado.
+					</p>
+				</div>
+			</div>
+			<div
+				class="w-1/2 h-full flex flex-col items-center justify-center space-y-8 lg:w-2/6 md:ml-8 "
+			>
+				<img src="home1.png" alt="" class="h-20 md:h-28 lg:h-40" />
+				<img src="home2.png" alt="" class="h-20 md:h-28 lg:h-40" />
+			</div>
+		</div>
+	</section>
+</main>
+
+
+
+
